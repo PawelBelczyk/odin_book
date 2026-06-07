c22b4ed01089e51618b917f3c77fee360ad1f187
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-06-07 16:09:02 +0200

    added functionality posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.includes(:user).order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "Post created."
+    else
+      @posts = Post.includes(:user).order(created_at: :desc)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
