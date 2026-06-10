2fbae78425e7fb5fc76adafa2459a1f223f2512f
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-06-10 07:36:11 +0200

    image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :content, presence: true
+  validate :content_or_image
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_one_attached :image
+
+
+  private
+
+  def content_or_image
+  return if content.present? || image.attached?
+
+  errors.add(:base, "Post must have content or image")
+  end
 end
